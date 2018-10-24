2d1b7e7911b71a0746f10f9ac244fcd8196b9ff1
Maxim <Maxim.Perl@outlook.de>
2018-10-24 16:31:13 +0200

    basic crawler implementation

--- SoundBaseCollection/models.py
+++ SoundBaseCollection/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import*
 
 """ Hier kannst du weitere Felder hinzufügen, die in die Datenbank übernommen werden sollen.. Die Daten können auch erstmal recht grob eingelesenen werden. Sobald der Crawler läuft
     und wir mehr Daten in die Datenbank führen können, kümmere ich mich um das Data Cleansing und die Auswertung.
@@ -8,11 +9,49 @@ from django.db import models
     3. Nicht vergessen, das Model auch in views.py und admin.py zu importieren, wenn ein Zugriff darauf erfolgen sollen
     4. Anschließend auch python manage.py makemigrations und migrate nicht vergesse :) """
 
+### Data ist eine Zwischenlagerung der Crawler Elemente ###
 
 class ElectronicData(models.Model):
-    Artist = models.CharField(max_length=100)
-    song_title = models.CharField(max_length=50)
+    Artist = models.CharField(max_length=100, default='Unknown')
+    song_title = models.CharField(max_length=100)
     link = models.CharField(max_length=200)
+    comment_count = models.IntegerField(default=0)
+    likes_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=datetime.now)
+    last_modified = models.DateTimeField(default=datetime.now)
+    release_date = models.DateTimeField(default=datetime.now)
+    download_count = models.IntegerField(default=0)
+    duration = models.IntegerField(default=0)
+    full_duration = models.IntegerField(default=0)
+    playback_count = models.IntegerField(default=0)
+    reposts_count = models.IntegerField(default=0)
+    tag_list = models.CharField(max_length=3000, default='Electronic, Deep House')
+    uri = models.CharField(max_length=400, default='Not included')
+    used = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.song_title
+
+
+### Base ist die tatsächlich genutzte Datenbank
+
+class ElectronicBase(models.Model):
+    Artist = models.CharField(max_length=100, default='Unknown')
+    song_title = models.CharField(max_length=100)
+    link = models.CharField(max_length=200)
+    comment_count = models.IntegerField(default=0)
+    likes_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=datetime.now)
+    last_modified = models.DateTimeField(default=datetime.now)
+    release_date = models.DateTimeField(default=datetime.now)
+    download_count = models.IntegerField(default=0)
+    duration = models.IntegerField(default=0)
+    full_duration = models.IntegerField(default=0)
+    playback_count = models.IntegerField(default=0)
+    reposts_count = models.IntegerField(default=0)
+    tag_list = models.CharField(max_length=3000, default='Electronic, Deep House')
+    uri = models.CharField(max_length=400, default='Not included')
+    used = models.IntegerField(default=0)
 
     def __str__(self):
         return self.song_title
--- SoundBaseCollection/views.py
+++ SoundBaseCollection/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import View
 from .models import ElectronicData
+from .crawl_classes import Validator, MainClass, Flatfiler, Crawler
+import ssl
+import json
 
 
 # In diesem View kann der Crawler integriert werden.
@@ -9,17 +12,49 @@ class IndexView(View):
 
     # Das ist die Baustelle für den Crawler
     def post(self, request):
+        # Post Daten einlesen und keywords in Liste convertieren
+        search_token = request.POST['search'].lower().split(',')
 
-        """ Der Crawler sollte in diese Post Funktion eingegliedert werden. Die Funktionen, die du erstellt hast, können erhalten bleiben
-            Am Ende muss nur die Post Funktion die ausführende sein. Das sollte eigentlich kein Problem darstellen
-            Interessant ist, wie die Performance das ausgleicht.. Mit wenn das Grundsätzlich läuft können wir auch die Ergebnisse tracken, das wird aber ein bisschen Fummelarbeit mit Java Script."""
+        # SSL Zertifikatsfehler ignorieren
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
 
-        search_token = request.POST['search']
-        pass
+        flatfiler = Flatfiler()
+        initial_links = flatfiler.get_initial_links(search_token)
+        print(initial_links)
+
+        # Instanzierung des Crawler-Objektes
+        crawler = Crawler(test_run = True)
+
+        # Erstellen einer Results-Liste und pflegt die Tracks in die Data (Zwischendatenbank ein)
+        crawl_results = crawler.scrape(initial_links)
+        for J_track in crawl_results:
+            #J_track = json.dumps(track)
+            song = ElectronicData()
+            # song.Artist = J_track['track_json']['publisher_metadata']['artist']
+            song.song_title = J_track['track_json']['title']
+            song.link = J_track['track_json']['permalink_url']
+            song.comment_count = J_track['track_json']['comment_count']
+            song.likes_count = J_track['track_json']['likes_count']
+            # song.created_at = J_track['track_json']['created_at']
+            # song.last_modified = J_track['track_json']['last_modified']
+            # song.release_date = J_track['track_json']['release_date']
+            song.download_count = J_track['track_json']['download_count']
+            song.duration = J_track['track_json']['duration']
+            song.full_duration = J_track['track_json']['full_duration']
+            song.playback_count = J_track['track_json']['playback_count']
+            song.reposts_count = J_track['track_json']['reposts_count']
+            song.tag_list = J_track['track_json']['tag_list']
+            song.uri = 'https://w.soundcloud.com/player/?url=https%3A//{}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'.format(J_track['track_json']['uri'][8:])
+
+            song.save()
+
+        return render(request, self.template_name, {'sum': len(crawl_results)})
 
     # Diese Funktion ruft einfach nur den Zustand und muss deshalb auch nicht mehr angepasst werden.
     def get(self, request):
-        return render(request, self.template_name)
+        return render(request, self.template_name, {'sum':0})
 
 # Der View kann erstmal unverändert bleiben, der der Fokus auf der Crawler integration und dem ML-Algorythmus liegt.
 class ElectroView(View):
@@ -28,10 +63,15 @@ class ElectroView(View):
     datapoint = ElectronicData.objects.all()
 
     # Arbeitsschritt für später
-    def post(self, request):
+    def post(self, request, song_id):
         """ An dieser Stelle wird der Postman integriert. Wenn sich für eine Verföffentlichung entschieden wurde muss der Request ausgeführt werden. Ob mit API oder ohne.
             Bevor das Script allerdings in den View eingebettet wird, sollte es über ein normales Rohscript funktionieren, damit wir hier nicht alles zumüllen. """
-        pass
+
+        track = ElectronicData.objects.get(id=song_id)
+        track.used = 1
+        track.save()
+
+        return render(request, self.template_name, {'datapoint':self.datapoint, 'Genere':'Electronic'})
 
     # Diese Funktion ruft einfach nur den Zustand und muss deshalb auch nicht mehr angepasst werden.
     def get(self, request):
